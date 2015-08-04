7b8f2c7db1b074cd14388f0098b9a02b274bd381
dcumin39 <cumings@gmail.com>
2015-08-04 17:02:31 -0400

    Example Refactoring
    Refactored to simplify example

--- RenderHighCharts.Presentation/Controllers/HomeController.cs
+++ RenderHighCharts.Presentation/Controllers/HomeController.cs
@@ -13,9 +13,19 @@ namespace RenderHighCharts.Controllers
     {
         public ActionResult Index()
         {
-            HighChartsRequestService serivce = new HighChartsRequestService();
-            var bytes = serivce.RequestGraph("image/png", chart: new HighCharts("User Engagement")
+            HighChartsRequestService service = new HighChartsRequestService();
+            var highChartsData = GetUserEngagementHighChartsData();
+            var bytes = service.RequestGraph("image/png", highChartsData);
 
+            return File(bytes, "image.png");
+        }
+
+        private static HighCharts GetUserEngagementHighChartsData()
+        {
+            var lightColor = "#748c9c";
+            var grayColor = "#CCCCCC";
+
+            return new HighCharts("User Engagement")
             {
 
                 async = false,
@@ -25,13 +35,11 @@ namespace RenderHighCharts.Controllers
                   
                     yAxis = new HighChartsYAxis()
                     {
-                        title = new HighChartsTitle() { text = "Number of Occurences" },gridLineColor = "#CCC",
+                        title = new HighChartsTitle() { text = "Number of Occurences" },gridLineColor = grayColor,
                         labels = new HighChartsAxisLabels()
                         {
-                            style = new HighChartsStyle() { color = "#748c9c", fontWeight = "normal"}
+                            style = new HighChartsStyle() { color = lightColor, fontWeight = "normal"}
                         }
-
-
 
                     },
                     xAxis = new HighChartsXAxis()
@@ -39,44 +47,38 @@ namespace RenderHighCharts.Controllers
                         categories = new List<string> { "Week of 7/24/2015", "Week of 7/31/2015", "Week of 8/04/2015" },
                         labels = new HighChartsAxisLabels()
                         {
-                            style = new HighChartsStyle() { color = "#748c9c", fontWeight = "normal" }
+                            style = new HighChartsStyle() { color = lightColor, fontWeight = "normal" }
                         }
 
                     },
                     series = new List<HighChartsSeries>{ new HighChartsSeries()
-                  {
-                      name="Applause",
-                      data = new List<double> {44.9,33 ,3},
-                      color="#CCCCCC"
-                  },
+                    {
+                        name="Applause",
+                        data = new List<double> {44.9,33 ,3},
+                        color=grayColor
+                    },
 
-                    new HighChartsSeries()
-                  {
-                        name = "Comments",
-                      data = new List<double> {2.9,52,10 },
-                      color="#748c9c"
-                  }
-                    ,
+                        new HighChartsSeries()
+                        {
+                            name = "Comments",
+                            data = new List<double> {2.9,52,10 },
+                            color=lightColor
+                        }
+                        ,
 
-                    new HighChartsSeries()
-                  {
-                        name = "Logins",
-                      data = new List<double> {27,4,22 },
-                      color="#748c9c"
-                  }
+                        new HighChartsSeries()
+                        {
+                            name = "Logins",
+                            data = new List<double> {27,4,22 },
+                            color=lightColor
+                        }
                     }
-
 
                 },
                 type = HighChartsExportFormat.png
                
 
-
-            });
-
-            return File(bytes, "image.png");
+            };
         }
-
-
     }
 }
