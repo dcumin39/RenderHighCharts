e54926546ab650d29faa2632769ea08c655effe6
dcumings <dcumings@terryberry.com>
2015-08-18 16:25:22 -0400

    Better Error Messages

--- RenderHighCharts.Presentation/Controllers/HomeController.cs
+++ RenderHighCharts.Presentation/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using NLog;
 using RenderHighCharts.Constants;
 using RenderHighCharts.Entities;
 using RenderHighCharts.Services;
@@ -11,10 +12,12 @@ namespace RenderHighCharts.Controllers
 {
     public class HomeController : Controller
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
         public ActionResult Index()
         {
             HighChartsRequestService service = new HighChartsRequestService();
             var highChartsData = GetUserEngagementHighChartsData();
+            logger.Debug(Newtonsoft.Json.JsonConvert.SerializeObject(highChartsData));
             var bytes = service.RequestGraph("image/png", highChartsData);
 
             return File(bytes, "image.png");
@@ -22,61 +25,82 @@ namespace RenderHighCharts.Controllers
 
         private static HighCharts GetUserEngagementHighChartsData()
         {
-            var lightColor = "#748c9c";
-            var grayColor = "#CCCCCC";
 
+            var highChartsStyle = new HighChartsStyle() { color = "#000000", fontWeight = "normal" };
+
+       
             return new HighCharts("User Engagement")
             {
 
                 async = false,
                 content = "options",
+                title = new HighChartsTitle() { text = "" },
+
                 options = new HighChartsOptions(HighChartType.Spline)
                 {
-                  
-                    yAxis = new HighChartsYAxis()
-                    {
-                        title = new HighChartsTitle() { text = "Number of Occurences" },gridLineColor = grayColor,
-                        labels = new HighChartsAxisLabels()
-                        {
-                            style = new HighChartsStyle() { color = lightColor, fontWeight = "normal"}
-                        }
 
-                    },
+                   
                     xAxis = new HighChartsXAxis()
                     {
-                        categories = new List<string> { "Week of 7/24/2015", "Week of 7/31/2015", "Week of 8/04/2015" },
+                        gridLineWidth = 0,
+                        categories = new List<string> {   "Week of 7/25/2015",
+                                                            "Week of 8/1/2015",
+                                                            "Week of 8/8/2015",
+                                                            "Week of 8/15/2015"},
                         labels = new HighChartsAxisLabels()
                         {
-                            style = new HighChartsStyle() { color = lightColor, fontWeight = "normal" }
+                            align = "center",
+                            format = "{value}",
+                            formatter = null,
+                            autoRotation = "[-45]",
+                            rotation = null,
+                            style = highChartsStyle,
+                            y = null,
+                            x = null
+
                         }
+
+
+                    },
+                    yAxis = new HighChartsYAxis()
+                    {
+
+                        gridLineWidth = 1,
+                        labels = new HighChartsAxisLabels()
+                        {
+                            align = "center",
+                            format = "{value}",
+                            formatter = null,
+                            autoRotation = "[-45]",
+                            rotation = null,
+                            style = highChartsStyle,
+                            y = null,
+                            x = null
+
+                        },
+                        title = new HighChartsTitle() { text = "" },
+                        gridLineColor = "#8C8C8C",
 
                     },
                     series = new List<HighChartsSeries>{ new HighChartsSeries()
                     {
-                        name="Applause",
-                        data = new List<double> {44.9,33 ,3},
-                        color=grayColor
+                        name="Logins",
+                        data = new List<int> {   24,
+               30,
+               22,
+               30},
+                        color="#2D5F91",type=""
                     },
+                    
 
-                        new HighChartsSeries()
-                        {
-                            name = "Comments",
-                            data = new List<double> {2.9,52,10 },
-                            color=lightColor
-                        }
-                        ,
-
-                        new HighChartsSeries()
-                        {
-                            name = "Logins",
-                            data = new List<double> {27,4,22 },
-                            color=lightColor
-                        }
-                    }
+                      
+                    },credits = new HighChartsCredits() { enabled = false}
+                    
 
                 },
-                type = HighChartsExportFormat.png
-               
+                type = HighChartsExportFormat.png,width = 500,scale=null,constr = null,
+                callback = @"function(chart) {                        chart.setTitle({text:''});                            };"
+
 
             };
         }
--- RenderHighCharts/Services/HighChartsRequestService.cs
+++ RenderHighCharts/Services/HighChartsRequestService.cs
@@ -1,52 +1,79 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net;
+using System.Text;
 using RenderHighCharts.Entities;
 
 namespace RenderHighCharts.Services
 {
-    public  class HighChartsRequestService
+    public class HighChartsRequestService
     {
-        public  int MaxBytesToRead { get; set; }= 500000;
+        public int MaxBytesToRead { get; set; } = 500000;
         public string HighChartsRequestUrl { get; set; } = "http://export.highcharts.com";
 
-        public  byte[] RequestGraph(string format, HighCharts chart)
+        public byte[] RequestGraph(string format, HighCharts chart)
         {
             var postData = chart.GetSerializedData();
 
 
 
             HttpWebRequest request =
-            (HttpWebRequest) WebRequest.Create(HighChartsRequestUrl);
+            (HttpWebRequest)WebRequest.Create(HighChartsRequestUrl);
 
             request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
             request.Method = "POST";
             using (var streamWriter = new StreamWriter(request.GetRequestStream()))
             {
- 
+
                 streamWriter.Write(postData);
                 streamWriter.Flush();
                 streamWriter.Close();
             }
 
-
-            using (HttpWebResponse response = (HttpWebResponse) request.GetResponse())
+            try
             {
-                // Get the stream associated with the response.
-                using (Stream receiveStream = response.GetResponseStream())
+
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
                 {
+                    // Get the stream associated with the response.
+                    using (Stream receiveStream = response.GetResponseStream())
+                    {
 
-                    return ReadBytesFromResponse(receiveStream);
+                        return ReadBytesFromResponse(receiveStream);
+                    }
+
                 }
-
             }
+            catch (WebException ex)
+            {
+                StringBuilder builder = new StringBuilder();
+                builder.Append($"Status:{ex.Status}");
+                if (ex.Response != null)
+                {
+                    // can use ex.Response.Status, .StatusDescription
+                    if (ex.Response.ContentLength != 0)
+                    {
+                        using (var stream = ex.Response.GetResponseStream())
+                        {
+                            using (var reader = new StreamReader(stream))
+                            {
+                               builder.Append($"\n\n Response: {reader.ReadToEnd()}");
+                            }
+                        }
+                    }
+                }
+                throw new Exception(builder.ToString(),ex);
+            }
+
         }
 
-        private  byte[] ReadBytesFromResponse(Stream receiveStream)
+        private byte[] ReadBytesFromResponse(Stream receiveStream)
         {
             byte[] imageBytes;
             using (BinaryReader br = new BinaryReader(receiveStream))
             {
-               
+
                 imageBytes = br.ReadBytes(MaxBytesToRead);
                 br.Close();
             }
